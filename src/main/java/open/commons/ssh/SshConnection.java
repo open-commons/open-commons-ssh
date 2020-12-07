0845727e7ff68b205ee9ec442eb2dd233eb620d2
parkjunhong <parkjunhong77@gmail.com>
2020-12-07 17:17:38 +0900

    라이센스 문구 추가

--- src/main/java/open/commons/ssh/SshConnection.java
+++ src/main/java/open/commons/ssh/SshConnection.java
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2020 Park Jun-Hong_(parkjunhong77/google/com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  *
  * This file is generated under this project, "open-commons-ssh".
  *
@@ -29,7 +45,7 @@ import com.jcraft.jsch.Session;
  * @version
  * @author Park_Jun_Hong_(fafanmama_at_naver_com)
  */
-public class SshConnection implements AutoCloseable {
+public class SshConnection implements IConnectionInfo, AutoCloseable {
 
     /** 서버 사용자 계정 */
     @NotNull
